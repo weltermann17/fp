881af0b6947c9907d5439b977593984d4c93f893
weltermann17 <weltermann17@yahoo.com>
2012-12-17 21:17:16 +0100

    parallel

--- FP/src/com/ibm/fp/scala/ParallelComputing.scala
+++ FP/src/com/ibm/fp/scala/ParallelComputing.scala
@@ -11,30 +11,21 @@ object ParallelComputing extends App {
   def measure(body: => Unit) = { val b = System.nanoTime; body; (System.nanoTime - b) / 1000000000.0 }
 
   def stress(l: S) = {
-
     l.foldLeft(0) { case (l, e) => l + e }
-
     l.map(_ + 42)
-
     l.foldLeft(0) { case (l, e) => l + e }
-
   }
 
-  def benchmark[A <: S](l: S) = {
+  def benchmark(l: S) = {
     var c = 0.0
-    for (i <- 1 to runs) {
-      if (0 == i % 1) println(i)
-      c += measure(stress(l))
-    }
+    for (i <- 1 to runs) { println(i); c += measure(stress(l)) }
     c / runs
   }
 
   val linear = (1 to max).toList
-
   val parallel = (1 to max).toList.par
 
   println("linear average : " + benchmark(linear) + " sec")
-
   println("parallel average : " + benchmark(parallel) + " sec")
 
 }
