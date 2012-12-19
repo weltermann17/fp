5136250d45fd883e657e6a1f477413ce960d050e
weltermann17 <weltermann17@yahoo.com>
2012-12-19 13:55:43 +0100

    renamedtable

--- FP/src/com/ibm/fp/scala/DeclarativeLanguage.scala
+++ FP/src/com/ibm/fp/scala/DeclarativeLanguage.scala
@@ -2,21 +2,21 @@ package com.ibm.fp.scala
 
 object DeclarativeLanguage extends App {
 
-  case class Employee(firstname: String, lastname: String, department: String, salary: Int, ismanager: Boolean)
+  final case class Employee(firstname: String, lastname: String, department: String, salary: Int, ismanager: Boolean)
 
-  val table = List(
+  val employees = List(
     Employee("Joe", "Smith", "Sales", 50000, false),
     Employee("Suzanne", "Harrison", "Sales", 40000, false),
     Employee("Mary", "Jones", "Sales", 100000, true),
     Employee("Ben", "Simpson", "Accounting", 90000, true),
     Employee("Paul", "Miller", "Accounting", 25000, false))
 
-  println(table)
+  println(employees); println
 
-  val managers = table.filter(_.ismanager)
-  val highestsalaryofallmanagers = table.filter(_.ismanager).map(_.salary).sorted.last
-  val firstnameofbestpaidmanager = table.filter(_.ismanager).sortBy(_.salary).last.firstname
-  val employeesperdepartment = table.groupBy(_.department).map { case (k, v) => (k, v.size) }
+  val managers = employees.filter(_.ismanager)
+  val highestsalaryofallmanagers = employees.filter(_.ismanager).map(_.salary).sorted.last
+  val firstnameofbestpaidmanager = employees.filter(_.ismanager).sortBy(_.salary).last.firstname
+  val employeesperdepartment = employees.groupBy(_.department).map { case (k, v) => (k, v.size) }
 
   println(managers)
   println(highestsalaryofallmanagers)
